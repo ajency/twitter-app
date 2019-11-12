bca630931fdd43bcf8f3fa9f6288f39a36125dd2
Viraj Verlekar <viraj@ajency.in>
2019-11-12 18:23:49 +0530

    timer

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -9,7 +9,20 @@ export class SearchPageComponent implements OnInit {
 
   constructor() { }
 
+  timeLeft: number = 30;
+  interval;
+
   ngOnInit() {
+  	this.interval = setInterval(() => {
+      this.setTime();
+    },1000)
   }
 
+  setTime() {
+  	if(this.timeLeft > 1) {
+		this.timeLeft--;
+	} else {
+		this.timeLeft = 30;
+	}
+  }
 }
--- src/app/search-page/search-page.module.ts
+++ src/app/search-page/search-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SearchPageRoutingModule } from './search-page-routing.module';
 import { SearchPageComponent } from './search-page.component';
+import { SingleTweetComponent } from '../single-tweet/single-tweet.component';
 
 @NgModule({
-  declarations: [SearchPageComponent],
+  declarations: [SearchPageComponent,SingleTweetComponent],
   imports: [
     CommonModule,
     SearchPageRoutingModule
