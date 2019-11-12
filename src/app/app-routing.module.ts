6f13f08a36ca3d178a626db367e34878c6b36c03
Viraj Verlekar <viraj@ajency.in>
2019-11-12 17:43:29 +0530

    basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '',  loadChildren: './search-page/search-page.module#SearchPageModule'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
